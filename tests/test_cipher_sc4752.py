7cd71851ac10580d9e39d0fbf3b722b8435e1880
jiajia <71299133+SijiaChen0110@users.noreply.github.com>
2020-11-12 22:20:37 +0800

    add test

--- tests/test_cipher_sc4752.py
+++ tests/test_cipher_sc4752.py
@@ -3,3 +3,10 @@ from cipher_sc4752 import cipher_sc4752
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_cipher():
+    example='abc'
+    shift=3
+    expected='def'
+    actual=cipher_sc4752.cipher(example,shift)
+    assert actual==expected
